17fcafc10ade436a8177d06e83dd4b02d1db4706
ValentinoJerez <valenjerez69@gmail.com>
2025-02-13 19:56:09 -0300

    Completo

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const nombreAmigo = document.getElementById("amigo").value;
+    if (nombreAmigo.trim() === "") {
+        alert("¡Por favor, ingresa un nombre válido!");
+        return;
+    }
+
+    if (amigos.includes(nombreAmigo)) {
+        alert("¡Ese amigo ya está en la lista!");
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+    actualizarListaAmigos(); // Actualizar la lista
+    document.getElementById("amigo").value = "";
+}
+
+function actualizarListaAmigos() {
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = ""; // Limpiar la lista antes de actualizar
+
+    for (let i = 0; i < amigos.length; i++) {
+        const nombreAmigo = amigos[i];
+        const nuevoAmigo = document.createElement("li");
+        nuevoAmigo.textContent = nombreAmigo;
+        listaAmigos.appendChild(nuevoAmigo);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("¡Necesitas al menos dos amigos para hacer el sorteo!");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSecreto = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `
+        <li class="result-item">
+            <span class="result-name">${amigoSecreto}</span>
+        </li>
+    `;
+}
